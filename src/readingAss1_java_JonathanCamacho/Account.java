7352ead80d51365584ab084a7b8789373a674013
Jonathan Camacho <Jonathan Camacho@DESKTOP-4V427KT.fios-router.home>
2016-11-02 12:57:12 -0400

    Reading assignment question 1

---
+++ src/readingAss1_java_JonathanCamacho/Account.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package readingAss1_java_JonathanCamacho;
+
+import java.util.Date;
+
+/**
+ * @author Jonathan Camacho
+ *
+ */
+public class Account {
+	
+	private int id() {
+		return 0;
+	}
+	
+	private double balance() {
+		return 0;
+	}
+	
+	private double annualInterestRate() {
+		return 0;
+	}
+	
+	private Date dateCreated() {
+		return null;
+	}
+	
+	new Account();
+	
+	new Account(id,balance,annualInterestRate);
+	
+	accountAccess.dateCreated();
+	
+	private double getMonthlyInterestRate() {
+		c=balance*monthlyInterestRate.monthlyInterestRate is(annualInterestRate/12);
+		return c;
+	}
+	
+	private double withdraw() {
+		return 0;
+	}
+	
+	private double deposit() {
+		return 0;
+	}
+	
+	
+}
